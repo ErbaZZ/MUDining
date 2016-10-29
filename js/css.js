adfb8948689d3099cc912f748665bc55aeae15f5
Thatchapon Unprasert <poomst@hotmail.com>
2016-10-29 15:06:43 +0700

    JS to reduce the code.

---
+++ js/css.js
@@ -0,0 +1,5 @@
+document.write('\
+<link rel="stylesheet" media="screen" href="assets/css/bootstrap.css">\
+<link rel="stylesheet" type="text/css" href="assets/css/animate-custom.css" />\
+<link rel="stylesheet" type="text/css" href="assets/css/style.css" />\
+');
---
+++ js/header.js
@@ -0,0 +1,5 @@
+document.write('\
+<h1 id="banner">\
+  <a href="index.html">MU <strong>Dining</strong></a>\
+</h1>\
+');
