d4d4cedbabbf1d1303f6844fd483cb0fc40badad
Thatchapon Unprasert <poomst@hotmail.com>
2016-10-29 19:03:40 +0700

    Fix up.

--- js/header.js
+++ js/header.js
@@ -2,10 +2,10 @@ document.write('\
 <div id="banner" class="fixed-top">\
   <h1><a href="index.html">MU <strong>Dining</strong></a></h1>\
   <nav><ul>\
-			<li><a href="#">home</a></li>\
+			<li><a href="index.html">home</a></li>\
 			<li><a href="#">restaurants</a></li>\
 			<li><a href="#">reviews</a></li>\
-			<li><a href="#">contact</a></li>\
+			<li><a href="contact.html">contact</a></li>\
 		</ul></nav>\
 </div>\
 ');
