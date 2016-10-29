466c9863ba7bc2dc2ae1692a81ef3be9eba2e195
Thatchapon Unprasert <poomst@hotmail.com>
2016-10-29 15:42:39 +0700

    Prototyping banner menus.

--- js/css.js
+++ js/css.js
@@ -2,4 +2,5 @@ document.write('\
 <link rel="stylesheet" media="screen" href="assets/css/bootstrap.css">\
 <link rel="stylesheet" type="text/css" href="assets/css/animate-custom.css" />\
 <link rel="stylesheet" type="text/css" href="assets/css/style.css" />\
+<link rel="stylesheet" type="text/css" href="assets/css/banner-menu.css" />\
 ');
--- js/header.js
+++ js/header.js
@@ -1,5 +1,11 @@
 document.write('\
 <h1 id="banner">\
   <a href="index.html">MU <strong>Dining</strong></a>\
+  <nav><ul>\
+			<li class="active"><a href="#">home</a></li>\
+			<li><a href="#">restaurants</a></li>\
+			<li><a href="#">reviews</a></li>\
+			<li><a href="#">contact</a></li>\
+		</ul></nav>\
 </h1>\
 ');
