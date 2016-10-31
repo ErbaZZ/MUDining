2c87837257e8f8e7bd620e729e85f01bfbf754a7
ErbaZZ <weerawat.paw@gmail.com>
2016-10-31 11:42:08 +0700

    Register system implemented

--- auth.php
+++ auth.php
@@ -1,12 +1,38 @@
-<html>
-<head>
-  <script src="js/css.js"></script>
-</head>
-<body>
-  <script src="js/header.js"></script>
+<?php
+  $dbServer = "localhost";
+  $dbUser = "root";
+  $dbPass = "";
+  $dbName = "mudining";
+  $db = new mysqli($dbServer, $dbUser, $dbPass, $dbName);
 
-  <div id="wrapper" class="container">
-  </div>
+  // check connection
+  if ($db->connect_error) {
+    die("Connection failed: " . $db->connect_error);
+  }
 
-</body>
-</html>
+  if(!isset($_POST['username'])) {
+    header("LOCATION: register.php");
+  }
+
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+
+  // Testing MySQLi queries
+  $db->query("CREATE TABLE IF NOT EXISTS User (UserID int NOT NULL AUTO_INCREMENT PRIMARY KEY, Username varchar(255) NOT NULL UNIQUE, Password varchar(255) NOT NULL UNIQUE, FirstName varchar(255), LastName varchar(255), Nickname varchar(255), Gender character(1), Email varchar(255));");
+//  $fn = array("John","John","Tim","James");
+//  $ln = array("Smith","Ba","Beagle","Bond");
+//  $nn = array("JS","JB","TB","JB");
+//  for ($i=0; $i<sizeof($fn); $i++) {
+//    // ***Problematic*** table name has to be in *lower case*, variables in VALUES have to be 'quoted'
+    if(!$db->query("INSERT INTO user(Username, Password) VALUES ('$username','$password');")) {
+      echo "Failed to add user " . $fn[$i] . "<br />";
+    }
+//  }
+  $data = $db->query("SELECT * FROM user;");
+  while($row = $data->fetch_assoc())
+  {
+    echo $row['UserID'] . " " . $row['Username'] . " " . $row['Password'];
+    echo "<br />";
+  }
+  $db->close();
+?>
--- register.php
+++ register.php
@@ -1,15 +1,3 @@
-<?php
-  $dbServer = "localhost";
-  $dbUser = "root";
-  $dbPass = "";
-  $dbName = "mudining";
-  $db = new mysqli($dbServer, $dbUser, $dbPass, $dbName);
-
-  // check connection
-  if ($db->connect_error) {
-    die("Connection failed: " . $db->connect_error);
-  }
-?>
 <html>
 <head>
   <script src="js/css.js"></script>
@@ -22,10 +10,10 @@
 
     <div id="register" class="animate form">
       <span class="head">Registration</span>
-      <form name="input" action="auth.php" method="get">
+      <form name="input" action="auth.php" method="POST">
     		<table align="center">
           <tr><td class="formLabel">Username:</td><td><input type="text" name="username" pattern="[A-Za-z0-9]{1,}" title="Letters or numbers only" required></td></tr>
-          <tr><td class="formLabel">Password:</td><td><input type="text" name="password" pattern="[A-Za-z0-9]{1,}" title="Letters or numbers only" required></td></tr>
+          <tr><td class="formLabel">Password:</td><td><input type="password" name="password" pattern="[A-Za-z0-9]{1,}" title="Letters or numbers only" required></td></tr>
     			<tr><td class="formLabel">First name:</td><td><input type="text" name="firstname" pattern="[A-Za-z]{1,}" title="Letters only" required></td></tr>
     			<tr><td class="formLabel">Last name:</td><td><input type="text" name="lastname" pattern="[A-Za-z]{1,}" title="Letters only" required></td></tr>
     			<tr><td class="formLabel">Nickname:</td><td><input type="text" name="nicktname" pattern="[A-Za-z]{1,}" title="Letters only"></td></tr>
@@ -50,29 +38,6 @@
     		<br/><br/>
     	</form>
     </div>
-    <?php
-    // Testing MySQLi queries
-      $db->query("DROP TABLE IF EXISTS User");
-      $db->query("CREATE TABLE IF NOT EXISTS User (UserID int NOT NULL AUTO_INCREMENT PRIMARY KEY, Username varchar(255) NOT NULL UNIQUE, Password varchar(255) NOT NULL UNIQUE, FirstName varchar(255), LastName varchar(255), Nickname varchar(255), Gender character(1), Email varchar(255));");
-      $fn = array("John","John","Tim","James");
-      $ln = array("Smith","Ba","Beagle","Bond");
-      $nn = array("JS","JB","TB","JB");
-      for ($i=0; $i<sizeof($fn); $i++) {
-        // ***Problematic*** table name has to be in *lower case*, variables in VALUES have to be 'quoted'
-        if(!$db->query("INSERT INTO user(Username, Password, FirstName, LastName, Nickname) VALUES ('$fn[$i]','$fn[$i]','$fn[$i]','$ln[$i]','$nn[$i]');")) {
-          echo "Failed to add user " . $fn[$i] . "<br />";
-        }
-      }
-      $data = $db->query("SELECT * FROM user;");
-
-      while($row = $data->fetch_assoc())
-      {
-        echo $row['UserID'] . " " . $row['Username'] . " " . $row['LastName'];
-        echo "<br />";
-      }
-      $db->close();
-    ?>
-
   </div>
 </body>
 </html>
