2df19af85441ebc5aa15cc859672e6ac061e0ace
Thatchapon Unprasert <poomst@hotmail.com>
2016-11-01 18:19:54 +0700

    Minor edit.

--- js/header.js
+++ js/header.js
@@ -20,36 +20,36 @@ document.write('\
       <ul class="nav navbar-nav navbar-right">\
         <li class="dropdown">\
           <a href="#" class="dropdown-toggle" data-toggle="dropdown">Login <span class="caret"></span></a>\
-			<ul id="login-dp" class="dropdown-menu">\
-				<li>\
-					 <div class="row">\
-							<div class="col-md-12">\
-								 <form class="form" role="form" method="post" action="lauth.php" accept-charset="UTF-8" id="login-nav">\
-										<div class="form-group">\
-											 <label class="sr-only" for="usernamebox">Username</label>\
-											 <input type="text" class="form-control" id="usernamebox" name="username" placeholder="Username" required>\
-										</div>\
-										<div class="form-group">\
-											 <label class="sr-only" for="passwordbox">Password</label>\
-											 <input type="password" class="form-control" id="passwordbox" name="password" placeholder="Password" required>\
-                                             <div class="help-block text-right"><a href="#">Forget the password?</a></div>\
-										</div>\
-										<div class="form-group">\
-											 <button type="submit" class="btn btn-primary btn-block">Sign in</button>\
-										</div>\
-										<div class="checkbox">\
-											 <label>\
-											 <input type="checkbox" name="keeplogin">Keep me logged in\
-											 </label>\
-										</div>\
-								 </form>\
-							</div>\
-							<div class="bottom text-center">\
-								New here ? <a href="register.php"><b>Register</b></a>\
-							</div>\
-					 </div>\
-				</li>\
-			</ul>\
+    			<ul id="login-dp" class="dropdown-menu">\
+    				<li>\
+    					 <div class="row">\
+    							<div class="col-md-12">\
+    								 <form class="form" role="form" method="post" action="lauth.php" accept-charset="UTF-8" id="login-nav">\
+    										<div class="form-group">\
+    											 <label class="sr-only" for="usernamebox">Username</label>\
+    											 <input type="text" class="form-control" id="usernamebox" name="username" placeholder="Username" required>\
+    										</div>\
+    										<div class="form-group">\
+    											 <label class="sr-only" for="passwordbox">Password</label>\
+    											 <input type="password" class="form-control" id="passwordbox" name="password" placeholder="Password" required>\
+                                                 <div class="help-block text-right"><a href="#">Forget the password?</a></div>\
+    										</div>\
+    										<div class="form-group">\
+    											 <button type="submit" class="btn btn-primary btn-block">Sign in</button>\
+    										</div>\
+    										<div class="checkbox">\
+    											 <label>\
+    											 <input type="checkbox" name="keeplogin">Keep me logged in\
+    											 </label>\
+    										</div>\
+    								 </form>\
+    							</div>\
+    							<div class="bottom text-center">\
+    								New here ? <a href="register.php"><b>Register</b></a>\
+    							</div>\
+    					 </div>\
+    				</li>\
+    			</ul>\
         </li>\
       </ul>\
     </div>\
