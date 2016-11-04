31c5c1b71d622d088901588a37db6f123a90a980
Thatchapon Unprasert <poomst@hotmail.com>
2016-11-04 21:22:55 +0700

    Working horizontal scrolling.

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <html>
 <head>
   <script src="js/css.js"></script>
+  <script type='text/javascript' src='js/jquery.mousewheel.min.js'></script>
 </head>
 <body>
   <?php include("navbar.php"); ?>
@@ -12,11 +13,24 @@
 
   </div>
 
-  <div id="bottombar">
-      <div class="text">a</div>
-      <div class="text">s</div>
-      <div class="text">d</div>
-      <div class="text">f</div>
+  <div id="bottombar-wrapper">
+    <div id="bottombar">
+        <div class="text">a</div>
+        <div class="text">s</div>
+        <div class="text">d</div>
+        <div class="text">f</div>
+        <div class="text">g</div>
+    </div>
   </div>
+
 </body>
+<script>
+  $(document).ready(function () {
+     $("#bottombar-wrapper").mousewheel(function(event, delta) {
+       this.scrollLeft -= (delta * 80);
+       this.scrollRight -= (delta * 80);
+       event.preventDefault();
+     });
+  });
+</script>
 </html>
