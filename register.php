779174e0be4e75897a6f7dfc11929d2985a0bed4
Thatchapon Unprasert <poomst@hotmail.com>
2016-11-05 12:25:04 +0700

    Password confirmation + major flaw fixed.

--- register.php
+++ register.php
@@ -7,30 +7,34 @@
 
   if (isset( $_POST['username'])) {
     $username = $_POST['username'];
-    $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_SPECIAL_CHARS);
-  	$password = md5(md5($username)."UMIDINGN".md5($password));
-    $firstname = $_POST['firstname'];
-    $lastname = $_POST['lastname'];
-    $nickname = $_POST['nickname'];
-    $gender = $_POST['sex'];
-    $email = $_POST['email'];
+    if ($_POST['password'] == $_POST['cpassword']) {
+      $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_SPECIAL_CHARS);
+    	$password = md5(md5($username)."UMIDINGN".md5($password));
+      $firstname = $_POST['firstname'];
+      $lastname = $_POST['lastname'];
+      $nickname = $_POST['nickname'];
+      $gender = $_POST['sex'];
+      $email = $_POST['email'];
 
-    $con->query("CREATE TABLE IF NOT EXISTS user (
-      UserID int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-      Username varchar(255) NOT NULL UNIQUE,
-      Password varchar(255) NOT NULL UNIQUE,
-      FirstName varchar(255),
-      LastName varchar(255),
-      Nickname varchar(255),
-      Gender character(1),
-      Email varchar(255));
-    ");
-    if (!$con->query("INSERT INTO user(Username, Password, FirstName, LastName, Nickname, Gender, Email) VALUES ('$username','$password','$firstname','$lastname','$nickname','$gender','$email');"))
-      $errormsg = "Username is already used.";
-    else
-      $successmsg = "Successfully Registered. Redirecting to home page.";
-      $_SESSION['Username'] = $username;
-      header("refresh:2;url=index.php");
+      $con->query("CREATE TABLE IF NOT EXISTS user (
+        UserID int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        Username varchar(255) NOT NULL UNIQUE,
+        Password varchar(255) NOT NULL UNIQUE,
+        FirstName varchar(255),
+        LastName varchar(255),
+        Nickname varchar(255),
+        Gender character(1),
+        Email varchar(255));
+      ");
+      if (!$con->query("INSERT INTO user(Username, Password, FirstName, LastName, Nickname, Gender, Email) VALUES ('$username','$password','$firstname','$lastname','$nickname','$gender','$email');"))
+        $errormsg = "Username is already used.";
+      else {
+        $successmsg = "Successfully Registered. Redirecting to home page.";
+        $_SESSION['Username'] = $username;
+        header("refresh:2;url=index.php");
+      }
+    } else
+      $errormsg = "Passwords do not match.";
     $con->close();
   }
 ?>
@@ -50,6 +54,7 @@
     		<table align="center">
           <tr><td class="formLabel">Username:</td><td><input type="text" name="username" pattern="[A-Za-z0-9]{1,}" title="Letters or numbers only" required></td></tr>
           <tr><td class="formLabel">Password:</td><td><input type="password" name="password" pattern="[A-Za-z0-9]{1,}" title="Letters or numbers only" required></td></tr>
+          <tr><td class="formLabel">Confirm:</td><td><input type="password" name="cpassword" pattern="[A-Za-z0-9]{1,}" title="Letters or numbers only" required></td></tr>
     			<tr><td class="formLabel">First name:</td><td><input type="text" name="firstname" pattern="[A-Za-z]{1,}" title="Letters only" required></td></tr>
     			<tr><td class="formLabel">Last name:</td><td><input type="text" name="lastname" pattern="[A-Za-z]{1,}" title="Letters only" required></td></tr>
     			<tr><td class="formLabel">Nickname:</td><td><input type="text" name="nickname" pattern="[A-Za-z]{1,}" title="Letters only"></td></tr>
