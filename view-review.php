e321d7ec2d1e97018c1432a408db8a81ddce4c77
Thatchapon Unprasert <poomst@hotmail.com>
2016-11-14 17:29:29 +0700

    Can delete a review.

--- view-review.php
+++ view-review.php
@@ -2,6 +2,8 @@
   include_once('dbconnect.php');
   if (!isset($_GET['id']))
     header('Location: index.php');
+  if (session_id() == '' || !isset($_SESSION))
+    session_start();
   $ID = $_GET['id'];
   $res = mysqli_fetch_assoc(mysqli_query($con, "select * from review where ReviewID = '$ID' LIMIT 1")) or header('Location: index.php');
 ?>
@@ -20,9 +22,15 @@
           echo $resname;
         ?>
       <br/>
-          <small>
-            test
-         </small>
+            <?php
+              if (isset($_SESSION['Username'])) {
+                $userID = mysqli_fetch_assoc(mysqli_query($con, 'select * from user where Username = "' . $_SESSION['Username'] . '" LIMIT 1'))['UserID'];
+                $result = mysqli_query($con, "select UserID from review where UserID = '$userID' and ReviewID = '$ID'");
+                if ($result->num_rows) {
+                  echo "<small><a href=\"delete-review.php\">Delete</a></small>";
+                }
+              }
+            ?>
       </h1>
       <hr/>
       <div>
