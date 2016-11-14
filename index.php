96c11efee71cc6b9adb28c0204b3539f565bda1d
Thatchapon Unprasert <poomst@hotmail.com>
2016-11-14 22:06:06 +0700

    Latest reviews on index page

--- index.php
+++ index.php
@@ -15,11 +15,26 @@
 
   <div id="bottombar-wrapper" class="navbar-fixed-bottom">
     <div id="bottombar">
-        <div class="text">a</div>
-        <div class="text">s</div>
-        <div class="text">d</div>
-        <div class="text">f</div>
-        <div class="text">g</div>
+      <?php
+        include_once("dbconnect.php");
+        $result = mysqli_query($con, "select * from review order by ReviewID desc limit 5");
+        while ($res = mysqli_fetch_assoc($result)) {
+          echo '<div class="text">';
+            echo '<div class="row">';
+              echo '<div class="col-xs-5">';
+                echo '<div class="thumbnail">';
+                  echo '<img src='."http://lorempixel.com/200/200/abstract/5/".'> style="height:auto;"';
+                echo '</div>';
+              echo '</div>';
+              echo '<div class="caption col-xs-7">';
+                echo "<h3>" . mysqli_fetch_assoc(mysqli_query($con, 'select * from restaurant where RestaurantID = ' . $res['RestaurantID'] . ' LIMIT 1'))['Name'] . "</h3>";
+                echo "<p>By " . mysqli_fetch_assoc(mysqli_query($con, 'select * from user where UserID = ' . $res['UserID'] . ' LIMIT 1'))['Username'] . "</p>";
+                $url = "view-review.php?id=" . $res['ReviewID'];
+                echo "<p><a href=\"$url\" class=\"btn btn-default\" role=\"button\">Read</a></p>";
+              echo '</div>';
+            echo '</div>';
+          echo '</div>';
+        }?>
     </div>
   </div>
 
