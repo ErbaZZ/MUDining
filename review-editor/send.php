e16f8ecc097b128215f5d798fc87edd4a3747758
ErbaZZ <weerawat.paw@gmail.com>
2016-11-15 14:45:38 +0700

    Duplicate review check.

--- review-editor/send.php
+++ review-editor/send.php
@@ -1,4 +1,5 @@
 <?php
+	session_start();
 	include_once("../dbconnect.php");
 	$username = $_POST['username'];
 	$restaurantName = $_POST['restaurantName'];
@@ -6,17 +7,24 @@
 	$data = $_POST['formData'];
 	$userID = mysqli_fetch_assoc(mysqli_query($con, "SELECT UserID FROM user WHERE user.Username = '$username' LIMIT 1"))['UserID'];
 	$restaurantID = mysqli_fetch_assoc(mysqli_query($con, "SELECT RestaurantID FROM restaurant WHERE restaurant.Name = \"".$restaurantName."\" LIMIT 1"))["RestaurantID"];
-	$reviewDate = date("Y-m-d H:i:s");
-	$query = "INSERT INTO review (UserID, RestaurantID, ReviewDate, Title, Content) VALUES ('$userID','$restaurantID','$reviewDate','$title','$data');";
-	echo "restaurantID = ".$restaurantID."</br>";
-	echo "userID = ".$userID."</br>";
-	if (!$con->query($query)) {
-		echo "Failed";
-		echo $con->error;
-		$errormsg = "You already reviewed"; // ?
+	$dupe = mysqli_fetch_assoc(mysqli_query($con, "SELECT * FROM review WHERE review.userID = '$userID' AND review.restaurantID = '$restaurantID' LIMIT 1"))['UserID'];
+	if (is_null($dupe)) {
+		$reviewDate = date("Y-m-d H:i:s");
+		$query = "INSERT INTO review (UserID, RestaurantID, ReviewDate, Title, Content) VALUES ('$userID','$restaurantID','$reviewDate','$title','$data');";
+		echo "restaurantID = ".$restaurantID."</br>";
+		echo "userID = ".$userID."</br>";
+		if (!$con->query($query)) {
+			echo "Failed";
+			echo $con->error;
+		}
+		else {
+			header('Location: ../reviews.php');
+		}
 	}
 	else {
-		header('Location: ../reviews.php');
+		$_SESSION['errormsg'] = "You have already reviewed this restaurant.";
+		echo $_SESSION['errormsg'];
+		header('Location: ../review-editor.php');
 	}
 
 ?>
