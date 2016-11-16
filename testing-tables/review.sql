321d3c736d3c8b021641af0439a89476f15274d1
ErbaZZ <weerawat.paw@gmail.com>
2016-11-16 13:33:53 +0700

    review table charset

--- testing-tables/review.sql
+++ testing-tables/review.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 09, 2016 at 08:08 AM
--- Server version: 10.1.13-MariaDB
--- PHP Version: 5.6.21
+-- Generation Time: Nov 16, 2016 at 06:26 AM
+-- Server version: 5.7.14
+-- PHP Version: 5.6.25
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -32,8 +32,8 @@ CREATE TABLE `review` (
   `RestaurantID` int(11) NOT NULL,
   `ReviewDate` date NOT NULL,
   `Title` varchar(64) NOT NULL,
-  `Content` mediumtext NOT NULL,
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `Content` mediumtext NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=tis620;
 
 --
 -- Dumping data for table `review`
@@ -42,9 +42,9 @@ CREATE TABLE `review` (
 INSERT INTO `review` (`ReviewID`, `UserID`, `RestaurantID`, `ReviewDate`, `Title`, `Content`) VALUES
 (1, 4, 1, '2016-11-02', 'Superb Review', 'The last choice for all. Ineffective, however.'),
 (2, 1, 2, '2016-11-04', 'Dummy Title', 'Never break any single dish.'),
-(3, 3, 3, '2016-11-01', 'Dummy Title2','System.out.println((new StringBuilder("MAHIDOL")).reverse())'),
-(4, 2, 4, '2016-11-05', 'Most comprehensive review of this restaurant ever','Huh?'),
-(5, 4, 5, '2016-11-02', 'Titled Review','Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod\r\n            tempor incididunt ut labore et dolore magna aliqua.');
+(3, 3, 3, '2016-11-01', 'Dummy Title2', 'System.out.println((new StringBuilder("MAHIDOL")).reverse())'),
+(4, 2, 4, '2016-11-05', 'Most comprehensive review of this restaurant ever', 'Huh?'),
+(5, 4, 5, '2016-11-02', 'Titled Review', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod\r\n            tempor incididunt ut labore et dolore magna aliqua.');
 
 --
 -- Indexes for dumped tables
@@ -64,7 +64,7 @@ ALTER TABLE `review`
 -- AUTO_INCREMENT for table `review`
 --
 ALTER TABLE `review`
-  MODIFY `ReviewID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `ReviewID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
