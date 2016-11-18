8ef7ef7dfe3f6ae49c7f90abd91bdfbe1bb29bbc
Thatchapon Unprasert <poomst@hotmail.com>
2016-11-18 18:38:13 +0700

    Good CSS tweak.

--- index.php
+++ index.php
@@ -20,19 +20,20 @@
         include_once("dbconnect.php");
         $result = mysqli_query($con, "select * from review order by ReviewID desc limit 5");
         while ($res = mysqli_fetch_assoc($result)) { ?>
-          <div class="text">
-            <div class="row">
-              <div class="col-lg-6 col-md-12 col-sm-12 col-xs-12">
-                <img class="thumbnail" id="latest-review-img" src=<?php echo imgurl($res['RestaurantID']) ?>>
-              </div>
-              <div class="caption col-lg-6 col-md-12 col-sm-12 col-xs-12">
-                <h3><?php echo mysqli_fetch_assoc(mysqli_query($con, 'select * from restaurant where RestaurantID = ' . $res['RestaurantID'] . ' LIMIT 1'))['Name'] ?></h3>
-                <p>By <?php echo mysqli_fetch_assoc(mysqli_query($con, 'select * from user where UserID = ' . $res['UserID'] . ' LIMIT 1'))['Username'] ?></p>
-                <p><?php echo $res['ReviewDate'] ?></p>
-                <a href=view-review.php?id=<?php echo $res['ReviewID'] ?> class="btn btn-default" role="button">Read</a>
+          <a href=view-review.php?id=<?php echo $res['ReviewID'] ?>>
+            <div class="text">
+              <div class="row">
+                <div class="col-lg-7 col-md-12 col-sm-12 col-xs-12" id="img-container">
+                  <img class="thumbnail" id="latest-review-img" src=<?php echo imgurl($res['RestaurantID']) ?>>
+                </div>
+                <div class="col-lg-5 col-md-12 col-sm-12 col-xs-12" id="img-caption">
+                  <h3><?php echo mysqli_fetch_assoc(mysqli_query($con, 'select * from restaurant where RestaurantID = ' . $res['RestaurantID'] . ' LIMIT 1'))['Name'] ?></h3>
+                  <p>By <?php echo mysqli_fetch_assoc(mysqli_query($con, 'select * from user where UserID = ' . $res['UserID'] . ' LIMIT 1'))['Username'] ?></p>
+                  <p><?php echo $res['ReviewDate'] ?></p>
+                </div>
               </div>
             </div>
-          </div>
+          </a>
         <?php } ?>
     </div>
   </div>
