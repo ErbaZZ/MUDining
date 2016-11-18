544f1226f77ee9eb1ad559fed17ebd7ecbffd0c1
ErbaZZ <weerawat.paw@gmail.com>
2016-11-18 16:02:41 +0700

    Add delete rating

--- rate.php
+++ rate.php
@@ -3,6 +3,11 @@
   $uid = $_GET['uid'];
   $rid = $_GET['rid'];
   $rate = $_GET['rate'];
-  if(!mysqli_query($con, "INSERT INTO rating (UserID, RestaurantID, Rating) VALUES ('$uid', '$rid','$rate')")) {
-    mysqli_query($con, "UPDATE rating SET Rating = '$rate' WHERE RestaurantID = '$rid' AND UserID = '$uid'");
+  if ($rate == 0) {
+    mysqli_query($con, "DELETE FROM rating WHERE RestaurantID = '$rid' AND UserID = '$uid'");
+  }
+  else {
+    if(!mysqli_query($con, "INSERT INTO rating (UserID, RestaurantID, Rating) VALUES ('$uid', '$rid','$rate')")) {
+      mysqli_query($con, "UPDATE rating SET Rating = '$rate' WHERE RestaurantID = '$rid' AND UserID = '$uid'");
+    }
   }
