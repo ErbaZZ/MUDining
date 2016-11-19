f577284438d34d4d5e25ec39fc07e29bd5093166
Thatchapon Unprasert <poomst@hotmail.com>
2016-11-19 20:53:19 +0700

    Works with index page.

--- dbconnect.php
+++ dbconnect.php
@@ -14,6 +14,13 @@
     return _imgurl($id, 1 + (rand() % 3));
   }
 
+  function getrevimage($con, $rev) {
+    require_once('fetch-img-review.php');
+    $imgs = getimages($con, $rev['ReviewID']);
+    array_push($imgs, imgurl($rev['RestaurantID']));
+    return $imgs[rand() % sizeof($imgs)];
+  }
+
   function isresopen($opentime, $closetime) {
     $currenttime = doubleval(date('H.i'));
     return $opentime <= $currenttime && $currenttime <= $closetime;
--- index.php
+++ index.php
@@ -23,17 +23,17 @@
       <?php
         include_once("dbconnect.php");
         $result = mysqli_query($con, "select * from review order by ReviewID desc limit 5");
-        while ($res = mysqli_fetch_assoc($result)) { ?>
-          <a href=view-review.php?id=<?php echo $res['ReviewID'] ?>>
+        while ($rev = mysqli_fetch_assoc($result)) { ?>
+          <a href=view-review.php?id=<?php echo $rev['ReviewID'] ?>>
             <div class="text">
               <div class="row">
                 <div class="col-lg-7 col-md-12 col-sm-12 col-xs-12" id="img-container">
-                  <img class="thumbnail" id="latest-review-img" src=<?php echo imgurl($res['RestaurantID']) ?>>
+                  <img class="thumbnail" id="latest-review-img" src=<?php echo getrevimage($con, $rev) ?>>
                 </div>
                 <div class="col-lg-5 col-md-12 col-sm-12 col-xs-12" id="img-caption">
-                  <h3><?php echo mysqli_fetch_assoc(mysqli_query($con, 'select * from restaurant where RestaurantID = ' . $res['RestaurantID'] . ' LIMIT 1'))['Name'] ?></h3>
-                  <p>By <?php echo mysqli_fetch_assoc(mysqli_query($con, 'select * from user where UserID = ' . $res['UserID'] . ' LIMIT 1'))['Username'] ?></p>
-                  <p><?php echo $res['ReviewDate'] ?></p>
+                  <h3><?php echo mysqli_fetch_assoc(mysqli_query($con, 'select * from restaurant where RestaurantID = ' . $rev['RestaurantID'] . ' LIMIT 1'))['Name'] ?></h3>
+                  <p>By <?php echo mysqli_fetch_assoc(mysqli_query($con, 'select * from user where UserID = ' . $rev['UserID'] . ' LIMIT 1'))['Username'] ?></p>
+                  <p><?php echo $rev['ReviewDate'] ?></p>
                 </div>
               </div>
             </div>
