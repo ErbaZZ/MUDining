d677c6365397c6215acaf19efd59f156fc878b2d
wakawaka2537 <wakawaka2537@gmail.com>
2016-11-20 09:05:54 +0700

    add descriptions 2 and 3

--- testing-tables/restaurants.sql
+++ testing-tables/restaurants.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.4
--- https://www.phpmyadmin.net/
+-- version 4.5.2
+-- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Generation Time: Nov 15, 2016 at 03:21 PM
--- Server version: 5.7.14
--- PHP Version: 5.6.25
+-- Host: localhost
+-- Generation Time: Nov 20, 2016 at 03:04 AM
+-- Server version: 10.1.16-MariaDB
+-- PHP Version: 7.0.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -45,22 +45,22 @@ CREATE TABLE `restaurant` (
 
 INSERT INTO `restaurant` (`RestaurantID`, `Name`, `MinPrice`, `MaxPrice`, `Location`, `Type`, `OpenTime`, `CloseTime`, `Promotion`, `Description`) VALUES
 (1, 'ไม่ตกไม่แตก', 51, 100, 'F', '01,04,11,12', 10.3, 21, NULL, ''),
-(2, 'สเต็กลุงหนวด', 0, 100, 'F', '04,11', 17, 23.3, NULL, ''),
-(3, 'J-Class Vegetarian', 51, 100, 'R', '01,03,11', 8, 20, NULL, ''),
+(2, 'สเต็กลุงหนวด', 0, 100, 'F', '04,11', 17, 23.3, NULL, 'There are many branches of Steak-lung-nhuad in Thailand, and the best one are in font of Mahidol University. There are many kind of steak such as chicken steak, pork chops ,fish steak, beef steak and hotdog. All steaks will be served with French fries.  You can order level of steak doneness with waiters.  Also, there are many dishes of spaghetti ,American fire rice, and Salad. For beverage, you can order Coke, or water.\n'),
+(3, 'J-Class Vegetarian', 51, 100, 'R', '01,03,11', 8, 20, NULL, 'The Vegetarian restaurant located across the road from Mahidol University near Namnuan restaurant. In the morning it will sell noodle, and late in the morning it will sell cook to order which all are vegetarian food.\n'),
 (4, 'พุทธรักษา เรสเทรอง', 251, 500, 'F', '01,03,04,11', 10, 20, NULL, ''),
 (5, 'LODIHAM', 50, 100, 'F', '01,04,11,12', 10, 21, NULL, ''),
 (6, 'Music Square', 101, 250, 'I', '01,04,11,12', 7.3, 20, NULL, 'Restaurant looks cute and full of nature. It is located in College of Music, Mahidol Salaya. This restaurant has both Thai food and European food with great service for lunch and dinner.'),
-(7, 'Anya\'s Place', 251, 500, 'F', '01,04,11', 11, 23, NULL, 'Anya?s Place is one of the restaurants in Nakornpathom. Food is high quality and very delicious with fair price. The service is very nice as they service their family.'),
+(7, 'Anya''s Place', 251, 500, 'F', '01,04,11', 11, 23, NULL, 'Anya?s Place is one of the restaurants in Nakornpathom. Food is high quality and very delicious with fair price. The service is very nice as they service their family.'),
 (8, 'Buta Grill', 251, 500, 'F', '02,04,11,12,13', 11.3, 21.3, NULL, 'It is a Grill restaurant that worth to go and it is suitable for sea food lover because of high quality. It provides great service and fresh food with tasty sauce in 199 Bath and 299 for sea food. These prices include the soft drink.'),
 (9, 'Hua Seng Hong', 501, 1000, 'F', '01,03,11,12', 10, 22, NULL, 'Hua Seng Hong is located in the opposite side of Mahidol University. In front of the restaurant has outstanding red label. Parking is available at in front of the restaurant and both sides of the restaurant. Hua Seng Hong is the Chinese food restaurant that has set and single dish, but the set is cheaper. The price has from hundred to thousand.'),
 (10, 'ธนัญธรก๋วยเตี๋ยวไก่มะระ', 0, 100, 'R', '01,11', 18.3, 1, NULL, 'This the most interesting restaurant for Kai Mara Noodle lover, but it opens quite late around 18.30 '),
-(11, 'Koh Lanta Pizzeria', 101, 250, 'F', '04,11,12', 11, 22, NULL, 'Lanta Pizzeria\'s pizza is crispy dough. You can choose topping that you want, but it already provides some topping. The price isn?t different from choosing topping by yourself. '),
+(11, 'Koh Lanta Pizzeria', 101, 250, 'F', '04,11,12', 11, 22, NULL, 'Lanta Pizzeria''s pizza is crispy dough. You can choose topping that you want, but it already provides some topping. The price isn?t different from choosing topping by yourself. '),
 (12, 'ปู๊ตี่บะหมี่เกี๊ยวกุ้ง', 0, 50, 'F', '03,11', 17, 23.59, NULL, 'The old restaurant which located in front of Srifa bakery sell noodle there around 10 years. The highlight is large shrimp wonton.'),
 (13, 'ศรีไทยขาหมู', 0, 100, 'S', '03,11', 9, 15, NULL, 'Kao Kha Moo with 75 Baht is worth to pay if you compare with the quantity. It servers with medium-boil egg.'),
 (14, 'ครัวการ์ตูน', 0, 100, 'S', '01,11', 9, 21, NULL, ''),
 (15, 'สุธารส', 0, 50, 'F', '01,11', 7, 20, NULL, 'It is the noodle restaurant. The highkight is pork which has a good smell. The price is fair with quality and quantity.'),
-(16, 'Buri Yummy', 0, 100, 'F', '01,02,04,11,12', 11, 22, NULL, 'This restaurant is for Mahidol\'s student. It has many menu for you include Thai and European food. Price isn\'t too expensive and food is delicious.'),
-(17, 'D\'Eiffel', 251, 500, 'F', '04,11,12', 11, 22, NULL, 'This restaurant make you feel like rating in the luxurious classic-style European restaurant.'),
+(16, 'Buri Yummy', 0, 100, 'F', '01,02,04,11,12', 11, 22, NULL, 'This restaurant is for Mahidol''s student. It has many menu for you include Thai and European food. Price isn''t too expensive and food is delicious.'),
+(17, 'D''Eiffel', 251, 500, 'F', '04,11,12', 11, 22, NULL, 'This restaurant make you feel like rating in the luxurious classic-style European restaurant.'),
 (18, 'ห้องนั่งเล่น', 0, 100, 'F', '01,11,12', 10, 20, NULL, 'It is a small restaurant. The high light is menu which has many set to choose with cheap price.'),
 (19, 'โกวเล็ก กุ้งอบวุ้นเส้น', 51, 250, 'F', '01,03,11', 16, 22.3, NULL, 'It is street food that has long queue. You can enjoy with original taste of Hongkong Suki, Dried fish-maw soup and the other menus.'),
 (20, 'Rit วังหลัง', 101, 250, 'F', '02,11,12', 11, 21, NULL, 'Sushi style Wanglang resturant with fair price suit for sushi lover.');
