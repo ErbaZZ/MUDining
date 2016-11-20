b67c53ff8099241d94f09b4ca78357ea0b7b5e25
Thatchapon Unprasert <poomst@hotmail.com>
2016-11-20 10:18:26 +0700

    Contact.

--- contact.php
+++ contact.php
@@ -13,8 +13,7 @@
         <figure id="book">
           <img class="circular" src="assets/img/admin/book.jpg" alt="book" />
           <figcaption>
-            <span id="def">Book</span>
-            <span id="new">Thitiwut Chamornmarn</span>
+            <span>Book</span>
           </figcaption>
         </figure>
       </div>
@@ -22,8 +21,7 @@
         <figure id="erbazz">
           <img class="circular" src="assets/img/admin/erbazz.jpg" alt="erbazz" />
           <figcaption>
-            <span id="def">Erbazz</span>
-            <span id="new">Weerawat Pawanawiwat</span>
+            <span>Erbazz</span>
           </figcaption>
         </figure>
       </div>
@@ -31,8 +29,7 @@
         <figure id="jeep">
           <img class="circular" src="assets/img/admin/jeep.jpg" alt="jeep" />
           <figcaption>
-            <span id="def">Jeep</span>
-            <span id="new">Dawit Chusetthagarn</span>
+            <span>Jeep</span>
           </figcaption>
         </figure>
       </div>
@@ -40,10 +37,33 @@
         <figure id="poomsmart">
           <img class="circular" src="assets/img/admin/poomsmart.jpg" alt="poomsmart" />
           <figcaption>
-            <span id="def">PSX</span>
-            <span id="new">Thatchapon Unprasert</span>
+            <span>PSX</span>
           </figcaption>
         </figure>
+      </div>
+      <div class="col-xs-3">
+        <ul class="pager">
+          <li><span>Thitiwut Chamornmarn</span></li>
+          <li><a href="mailto:thitiwut.cha@student.mahidol.ac.th">Contact</a></li>
+        </ul>
+      </div>
+      <div class="col-xs-3">
+        <ul class="pager">
+          <li><span>Weerawat Pawanawiwat</span></li>
+          <li><a href="mailto:weerawat.paw@student.mahidol.ac.th">Contact</a></li>
+        </ul>
+      </div>
+      <div class="col-xs-3">
+        <ul class="pager">
+          <li><span>Dawit Chusetthagarn</span></li>
+          <li><a href="mailto:dawit.chu@student.mahidol.ac.th">Contact</a></li>
+        </ul>
+      </div>
+      <div class="col-xs-3">
+        <ul class="pager">
+          <li><span>Thatchapon Unprasert</span></li>
+          <li><a href="mailto:thatchapon.unp@student.mahidol.ac.th">Contact</a></li>
+        </ul>
       </div>
     </div>
   </div>
